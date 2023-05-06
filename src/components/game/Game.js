040d4cd16a783e78db589d1f97c1a63674e6bfd8
ahmet <ahmet55uluisik05@gmail.com>
2023-05-06 14:10:01 +0300

    create react app

---
+++ src/components/game/Game.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Game.css";
+const Game = () => {
+  return (
+    <div id="game">
+      <div className="row">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+      <div className="row">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+      <div className="row">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
